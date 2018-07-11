ae30a2e9d39b94f2cb514a1ced06c7bf719c4f85
AJ <anish.sj@gmail.com>
2018-07-10 23:35:33 -0400

    test prog

--- src/com/aj/Main.java
+++ src/com/aj/Main.java
@@ -1,8 +1,27 @@
 package com.aj;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
+    static int NUMCOUNT = 10;
+
     public static void main(String[] args) {
-	// write your code here
+        Random r = new Random(0);
+        List<Integer> input = new LinkedList<>();
+	    for(int i = 0 ; i < NUMCOUNT; i++){
+	        input.add(r.nextInt());
+        }
+
+        //compute mean
+        int mean = input.stream().mapToInt(i -> i).sum();
+
+        System.out.println("#s are ");
+        input.stream().forEach(i -> System.out.println(i));
+        System.out.println("Mean is: " + mean);
     }
+
+
 }
